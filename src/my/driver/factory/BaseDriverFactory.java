992ab21a164e697c903120367921a0d4973dd9cd
Balaji prabu <complanboy@gmail.com>
2018-05-10 07:39:57 +0530

    Initial commit

---
+++ src/my/driver/factory/BaseDriverFactory.java
@@ -0,0 +1,44 @@
+package my.driver.factory;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
+
+public class BaseDriverFactory {
+
+   private static WebDriver driver = null;
+   private static final String FIREFOX = "firefox";
+   private static final String CHROME = "chrome";
+
+   public static WebDriver getWebDriver(final String browser){
+      if (browser.isEmpty()) {
+         //throw exception
+      }
+      if(driver != null){
+         return driver;
+      }
+      synchronized (WebDriver.class) {
+         if (driver == null) {
+            String browserLowerCase = browser.toLowerCase();
+            switch(browserLowerCase) {
+               case FIREFOX:
+                  driver = new FirefoxDriver();
+                  break;
+               case CHROME:
+                  System.setProperty("webdriver.chrome.driver", "chromedriver");
+                  driver = new ChromeDriver();
+                  break;
+            }
+         }
+      }
+      return driver;
+   }
+
+   public static WebDriver getWebDriver(){
+         if(driver != null)
+            return driver;
+         else
+            driver = new FirefoxDriver();
+         return driver;
+   }
+
+}
---
+++ src/my/pages/LandingPage.java
@@ -0,0 +1,58 @@
+package my.pages;
+
+import java.util.concurrent.TimeUnit;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.By.ByXPath;
+import org.openqa.selenium.NoSuchElementException;
+import org.openqa.selenium.By.ById;
+import org.openqa.selenium.firefox.FirefoxDriver;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
+
+public class LandingPage {
+	
+	public static void main(String [] args ) {
+		System.setProperty("webdriver.gecko.driver", "resources/geckodriver");
+		WebDriver driver = new FirefoxDriver();
+		driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
+		
+		driver.get("https://www.spicinemas.in/");
+		
+		
+		WebDriverWait wait = new WebDriverWait(driver, 10); 
+//		String userInputCitySelection = "coimbatore";
+//		WebElement currentCitySelectedElement = driver.findElement(By.xpath("//span[@class = 'app-icon current-city']//following-sibling::span"));
+//		
+//		if(userInputCitySelection.equals(currentCitySelectedElement.getText()))
+		WebElement downArrowButton = ifPresentReturnElementElseNull("//form[@id='cityForm']//b[@class='dropdown-arrow']",driver);
+		if(downArrowButton!= null) {
+		
+			downArrowButton.click();
+
+			ByXPath cbeElement = new ByXPath("//li[text()='coimbatore']");
+			driver.findElement(cbeElement).click();
+
+			
+
+			//		WebElement okButton = wait.until(ExpectedConditions.visibilityOf(driver.findElement(new ById("selectCity"))));
+			WebElement okButton = driver.findElement(new ById("selectCity"));
+			okButton.click();
+		}
+		
+		
+	}
+	
+	public static WebElement ifPresentReturnElementElseNull(String xpath, WebDriver driver) {
+		WebElement element = null;
+		try {
+			element = driver.findElement(By.xpath(xpath));
+		} catch(NoSuchElementException e) {
+			System.out.println("element not found");
+		}
+		return element;
+	}
+	
+}
